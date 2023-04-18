bc5e366a7a0e21667719a98ac17065e2d70fb4c5
joel danjuma <joeldanjuma@joels-MacBook-Air.local>
2023-04-18 16:08:58 +0100

    Updated main file

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from .database import engine
 from . import models
-from .routers import post, user, auth, provider
+from .routers import post, user, auth, provider, admin
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -11,6 +11,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(provider.router)
+app.include_router(admin.router)
 
 #Root page for the api
 @app.get("/")
